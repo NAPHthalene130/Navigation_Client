510dc289588bae8a4b28e95d48458ed89194b236
NAPHthalene130 <1301537275@qq.com>
2025-12-04 00:42:15 +0800

    fix:消灭了封建残余

--- out/build/GCC 13.1.0 x86_64-w64-mingw32/.qt/deploy_Navigation_31139d8622.cmake
+++
@@ -1,8 +0,0 @@
-include("H:/CODE/Trae/Navigation/Navigation_Client/out/build/GCC 13.1.0 x86_64-w64-mingw32/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/Navigation-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "H:/CODE/Trae/Navigation/Navigation_Client/out/build/GCC 13.1.0 x86_64-w64-mingw32/Navigation.exe"
-    GENERATE_QT_CONF
-)
